9e627a823fdfe458c78cbfb062f9a66a6956c8a9
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-05-23 10:09:52 +0000

    Start over inital commit

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    let datepicker = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
-    // secelt initialzation
-    let selects = document.querySelectorAll('select');
+    // select initialization
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    // collapsible initialzation
-    let collapsibles = document.querySelectorAll('.collapsible');
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
 });
